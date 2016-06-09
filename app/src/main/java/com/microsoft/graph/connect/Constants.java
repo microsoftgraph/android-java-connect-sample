21deffc81e4bccbf7deb99025fbd71a01c652d9d
Ricardo Loo <ricardol@microsoft.com>
2016-06-09 09:12:34 -0700

    Making Constants.CLIENT_ID not final so we can use it in tests

--- app/src/main/java/com/microsoft/graph/connect/Constants.java
+++ app/src/main/java/com/microsoft/graph/connect/Constants.java
@@ -4,10 +4,10 @@
  */
 package com.microsoft.graph.connect;
 
-interface Constants {
-    String AUTHORITY_URL = "https://login.microsoftonline.com/common";
+public class Constants {
+    public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
     // Update these two constants with the values for your application:
-    String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
-    String REDIRECT_URI = "http://localhost:8000";
-    String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
+    public static String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
+    public static final String REDIRECT_URI = "http://localhost:8000";
+    public static final String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
 }
