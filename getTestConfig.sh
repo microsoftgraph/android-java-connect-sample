7c1d599aff2c7e8f08b32aa70d068af742be2db8
Ricardo Loo <ricardol@microsoft.com>
2016-06-10 14:29:26 -0700

    Adding test_client_id to the script

--- getTestConfig.sh
+++ getTestConfig.sh
@@ -4,3 +4,7 @@ testConfig="{ \"test_client_id\": \"${test_client_id}\", \"test_username\": \"${
 echo $testConfig
 echo $testConfig > testConfig.json
 echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "test_client_id_1 is $BUILD_TEST_CLIENT_ID"
+echo "test_client_id_2 is $BUILD_test_client_id"
+echo "test_client_id_3 is $test_client_id"
+echo "test_client_id_4 is $TEST_CLIENT_ID"
