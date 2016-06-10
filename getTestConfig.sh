36ca673d5639b911d2d52e0b7526f804da095d03
Ricardo Loo <ricardol@microsoft.com>
2016-06-10 14:23:27 -0700

    Reading variables in bash

--- getTestConfig.sh
+++ getTestConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-testConfig="{ \"test_client_id\": $test_client_id, \"test_username\": \"$test_username\", \"test_password\": \"$test_password\" }"
+testConfig="{ \"test_client_id\": \"${test_client_id}\", \"test_username\": \"${test_username}\", \"test_password\": \"${test_password}\" }"
 echo $testConfig
 echo $testConfig > testConfig.json
-
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
