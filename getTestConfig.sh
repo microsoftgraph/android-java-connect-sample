c97905830294872d20b18d565318e191522818af
Ricardo Loo <ricardol@microsoft.com>
2016-06-10 14:31:31 -0700

    Adding test_username and test_password to script

--- getTestConfig.sh
+++ getTestConfig.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-testConfig="{ \"test_client_id\": \"${test_client_id}\", \"test_username\": \"${test_username}\", \"test_password\": \"${test_password}\" }"
+testConfig="{ \"test_client_id\": \"$TEST_CLIENT_ID\", \"test_username\": \"$TEST_USERNAME\", \"test_password\": \"$TEST_PASSWORD\" }"
 echo $testConfig
 echo $testConfig > testConfig.json
-echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
-echo "test_client_id_1 is $BUILD_TEST_CLIENT_ID"
-echo "test_client_id_2 is $BUILD_test_client_id"
-echo "test_client_id_3 is $test_client_id"
-echo "test_client_id_4 is $TEST_CLIENT_ID"
