789a1da27c8b6af571c3091cea369573a26625a5
Ricardo Loo <ricardol@microsoft.com>
2016-06-23 16:32:40 -0700

    Added attributes in layout to handle the following events:
    * FocusForward
    * FocusUp
    * FocusDown
    * FocusLeft
    * FocusRight

--- app/src/main/java/com/microsoft/graph/connect/Constants.java
+++ app/src/main/java/com/microsoft/graph/connect/Constants.java
@@ -7,7 +7,7 @@ package com.microsoft.graph.connect;
 public class Constants {
     public static final String AUTHORITY_URL = "https://login.microsoftonline.com/common";
     // Update these two constants with the values for your application:
-    public static String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
+    public static String CLIENT_ID = "3d0d8ee3-e097-4039-82f3-8c11b64c8412";
     public static final String REDIRECT_URI = "http://localhost:8000";
     public static final String MICROSOFT_GRAPH_API_ENDPOINT_RESOURCE_ID = "https://graph.microsoft.com/";
 }
