c8c65f5be922f05020b5cf295b026edeace8ae7a
Ricardo Loo <ricardol@microsoft.com>
2016-06-30 13:57:09 -0700

    Moving bash script file to androidTest folder

--- app/src/androidTest/getTestConfig.sh
+++ app/src/androidTest/getTestConfig.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-testConfig="{ \"test_client_id\": \"$TEST_CLIENT_ID\", \"test_username\": \"$TEST_USERNAME\", \"test_password\": \"$1\" }"
+testConfig="{
+    \"test_client_id\": \"$TEST_CLIENT_ID\",
+    \"test_username\": \"$TEST_USERNAME\",
+    \"test_password\": \"$1\"
+}"
 echo $testConfig
 echo $testConfig > testConfig.json
 
