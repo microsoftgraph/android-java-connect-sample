54cbf2c15d94be8811d13ac88d52dfbb47e27adf
John Austin <JohnAustin-MSFT@users.noreply.github.com>
2017-05-03 12:24:31 -0700

    Update Constants.java

--- app/src/main/java/com/microsoft/graph/connect/Constants.java
+++ app/src/main/java/com/microsoft/graph/connect/Constants.java
@@ -9,10 +9,10 @@ public class Constants {
     public static final String AUTHORIZATION_ENDPOINT = "/oauth2/v2.0/authorize";
     public static final String TOKEN_ENDPOINT = "/oauth2/v2.0/token";
     // Update these two constants with the values for your application:
-    public static String CLIENT_ID = "c435e172-4956-4db8-92c8-0c989f913d3e";
+    public static String CLIENT_ID = "ENTER_YOUR_CLIENT_ID";
     public static final String REDIRECT_URI =  "https://login.microsoftonline.com/common/oauth2/nativeclient";
     public static final String[] SCOPES = {"openid", "profile","Mail.ReadWrite","mail.send","Files.ReadWrite","User.ReadBasic.All"};
-    public static final String LOGIN_HINT = "johnau";
+    public static final String LOGIN_HINT = "alexd@contoso.com";
     public static final String EXTRA_QUERY_PARAM = null;
     public static final String[] ADDITIONAL_SCOPE = {"",""};
 }
