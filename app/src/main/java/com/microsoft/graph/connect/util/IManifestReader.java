26ba0b71ab4ab6667e9d7ba4d3d7d9138930fcba
Irvine Sunday <v-irsund@microsoft.com>
2019-05-02 17:56:36 +0300

    Implementing the manifest class and interface

--- app/src/main/java/com/microsoft/graph/connect/util/IManifestReader.java
+++ app/src/main/java/com/microsoft/graph/connect/util/IManifestReader.java
@@ -1,4 +1,16 @@
 package com.microsoft.graph.connect.util;
 
+import java.util.HashMap;
+
 public interface IManifestReader {
+    String getApplicationMetadataValueString(String key);
+    int getApplicationMetadataValueInt(String key);
+    boolean getApplicationMetadataValueBoolean(String key);
+    int getApplicationMetadataValueColor(String key);
+    float getApplicationMetadataValueFloat(String key);
+    String getIntentFilterAction(String activityName);
+    String[] getIntentFilterCategories(String activityName);
+    HashMap<String, String> getIntentFilterData(String activityName);
+
 }
+
--- app/src/main/java/com/microsoft/graph/connect/util/ManifestReader.java
+++ app/src/main/java/com/microsoft/graph/connect/util/ManifestReader.java
@@ -1,4 +1,77 @@
 package com.microsoft.graph.connect.util;
 
-public class ManifestReader {
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.microsoft.graph.connect.Connect;
+
+import java.util.HashMap;
+
+/*
+Contains methods that access the application manifest
+ */
+public class ManifestReader implements IManifestReader{
+
+    /**
+     * Gets the value of an AndroidManifest meta-data node. If the node value cannot be cast to String,
+     * null is returned.
+     * @param key String. The meta-data key value
+     * @return String.  The value associated with the key
+     */
+    @Override
+    public String getApplicationMetadataValueString(String key) {
+        String returnValue = "";
+        try {
+            PackageInfo info = Connect.getContext().getPackageManager().getPackageInfo(
+                    Connect.getContext().getPackageName(),
+                    PackageManager.GET_META_DATA);
+            if (info.applicationInfo.metaData != null) {
+                Bundle bundle = info.applicationInfo.metaData;
+                returnValue = bundle.getString(key);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+
+    @Override
+    public int getApplicationMetadataValueInt(String key) {
+        return 0;
+    }
+
+    @Override
+    public boolean getApplicationMetadataValueBoolean(String key) {
+        return false;
+    }
+
+    @Override
+    public int getApplicationMetadataValueColor(String key) {
+        return 0;
+    }
+
+    @Override
+    public float getApplicationMetadataValueFloat(String key) {
+        return 0;
+    }
+
+    @Override
+    public String getIntentFilterAction(String activityName) {
+        return null;
+    }
+
+    @Override
+    public String[] getIntentFilterCategories(String activityName) {
+        return new String[0];
+    }
+
+    @Override
+    public HashMap<String, String> getIntentFilterData(String activityName) {
+        return null;
+    }
+
+
 }
+
